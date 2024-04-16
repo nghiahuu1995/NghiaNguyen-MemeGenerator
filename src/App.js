06f432a0a081198b9ac2a44e92a08edde72527fc
Nghia Nguyen <ainzynguyen95@gmail.com>
2024-04-15 17:26:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState, useEffect } from 'react'
+// const API = "1e57f18139504747aca902e209cb551e"
 function App() {
+  const [text, setText] = useState({ top: "", bottom: "" });
+  const [imgURL, setImageURL] = useState("")
+  const onTextChanged = (UpdatedText) => {
+    setText({ ...text, ...UpdatedText })
+  }
+
+  async function fetchAPI() {
+    try {
+      const res = await fetch(`https://api.imgflip.com/get_memes`);
+      const data = await res.json();
+      if (data.success) {
+        const memes = data.data.memes;
+        const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+        setImageURL(randomMeme.url);
+      } else {
+        throw new Error('Failed to fetch memes');
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(function () {
+
+    fetchAPI();
+  }, [])
+  const handlerGenerator = () => {
+    fetchAPI()
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextBox onTextChanged={onTextChanged}>Text</TextBox>
+
+      <MemeImg text={text} src={imgURL} />
+      <Button handlerGenerator={handlerGenerator}>Generate</Button>
     </div>
   );
 }
-
+function TextBox({ children = '', onTextChanged }) {
+  return <div>
+    <h1>{children}</h1>
+    <label className="">{children}</label>
+    <input type="text" onChange={((e) => onTextChanged({ top: e.target.value }))} />
+    <input type="text" onChange={((e) => onTextChanged({ bottom: e.target.value }))} />
+  </div>
+}
+function Button({ children, handlerGenerator }) {
+  return <div className="container">
+    <button onClick={handlerGenerator} className="btn">{children}</button>
+  </div>
+}
+function MemeImg({ src = '', text }) {
+  return <div className="container">
+    <img src={src} alt="" />
+    <h1 className="top-text">{text.top}</h1>
+    <h1 className="bottom-text">{text.bottom}</h1>
+  </div>
+}
 export default App;
